d686317f0dd2555f7fc7cefbb5d84fc074196923
SpencerHorn <spencer.horn@smashingboxes.com>
2016-12-12 14:41:07 -0500

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,13 +24,18 @@ module SessionsHelper
     end
   end
 
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+
   # Forgets a persistent session.
   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
-  
+
   # Logs out the current user.
   def log_out
     forget(current_user)
